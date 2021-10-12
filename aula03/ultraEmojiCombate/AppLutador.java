0895c83a021353310046458d0f86486b429c5fe0
brunoSS07 <brunosss44@gmail.com>
2021-10-12 00:46:24 -0300

    feito o exercicio do combate

---
+++ aula03/ultraEmojiCombate/AppLutador.java
@@ -0,0 +1,21 @@
+package aula03.ultraEmojiCombate;
+
+
+public class AppLutador {
+    public static void main(String[] args) {
+        Lutador lutador[] = new Lutador[6];
+
+        lutador[0] = new Lutador("Prety Boy", "França", 31, 1.75, 68.9, 11, 3, 1);
+        lutador[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+        lutador[2] = new Lutador("Snapshadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
+        lutador[3] = new Lutador("Dead Code", "Australia", 28, 1.93, 81.6, 13, 0, 2);
+        lutador[4] = new Lutador("Uf0Cobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+        lutador[5] = new Lutador("Nerdaart", "EUA", 30, 1.81, 105.7, 12, 2, 4);
+
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(lutador[2], lutador[3]);
+        UEC01.lutar();
+        
+        
+    }
+}
---
+++ aula03/ultraEmojiCombate/Lutador.java
@@ -0,0 +1,122 @@
+package aula03.ultraEmojiCombate;
+
+public class Lutador {
+    private String nome, nacionalidade, categoria;
+    private double altura, peso;
+    private int vitorias, derrota, empates, idade;
+    
+
+    public Lutador(String nome, String nacionalidade, int idade, double altura, double peso, int vitorias, int derrotas, int empates){
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        this.altura = altura;
+        setPeso(peso); // irei analisar em qual CATEGORIA seu PESO se encaixa
+        this.vitorias = vitorias;
+        this.derrota = derrotas;
+        this.empates = empates;
+    }
+
+    public void apresentar(){
+        System.out.println("=============================================");
+       System.out.println(String.format("CHEGOU A HORA! Apresentamos o lutador %s\nDiretamente de(a,o) %s\ncom %d anos\npesando %.02f Kg\nPeso: %.02f\n%d vitorias\n%d derrotas\n%d empates", this.getNome(), this.getNacionalidade(), this.getIdade(), this.getAltura(), this.getPeso(), this.getVitorias(), this.getDerrota(),this.getEmpates()));
+    }
+    public void status(){
+        System.out.println("-------------------------");
+        System.out.println(this.getNome() + " é um peso " + this.getCategoria());
+        System.out.println("Ganhou " + this.getVitorias()+ " vezes");
+        System.out.println("Perdeu " + this.getDerrota() + " vezes");
+        System.out.println("Empatou " + this.getEmpates() + " vezes");
+    }
+    public void ganharLuta(){
+        this.setVitorias(this.getVitorias() + 1);
+    }
+    public void perderLuta(){
+        this.setDerrota(this.getDerrota() + 1);
+    }
+    public void empatarLuta(){
+        this.setEmpates(this.getDerrota() + 1);
+    }
+
+
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+        this.setCategoria();
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+    private void setCategoria() {
+        if(this.peso < 52.2){
+            categoria = "Inválido";
+        }else if(this.peso <= 70.3){
+            categoria = "Leve";
+        }else if(this.peso <= 83.9){
+            categoria = "Médio";
+        }else if(this.peso <= 120 ){
+            categoria = "Pesado";
+        }else{
+            categoria = "Inválido";
+        }
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrota() {
+        return derrota;
+    }
+    public void setDerrota(int derrota) {
+        this.derrota = derrota;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+}
+
+    
+
+
+
+
